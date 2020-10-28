95a31e49c7262a38a0772cd31e84f8489d5071e2
Thibault Polge <thibault@thb.lt>
2020-10-28 15:18:47 +0100

    Re-think Layers

--- src/KLMC/Types.hs
+++ src/KLMC/Types.hs
@@ -17,6 +17,8 @@ data Config = Config
   { name :: String }
    deriving (Eq, Ord, Read, Show)
 
+-- ** States
+
 -- | KLMC treats a layout as fundamentally a state machine, with a
 -- twist: there are two parallel state systems, States and Layers.
 -- States are used to implement dead keys: dead keys enable a state,
@@ -36,9 +38,44 @@ data State =
     , repr :: String }
     deriving (Eq, Ord, Read, Show)
 
--- | Most modifiers are duplicated left and right, and MacOS at least
--- supports this.
-data ModifierState = Off | Any | Left | Right
+-- | Layers are a complex thing, because they're really the most
+-- platform-specific of all abstractions.
+--
+-- XKB's layers are called levels.  Odd levels are the base level (1)
+-- and the higher levels (3, 5, 7); even levels are the shifted
+-- version of the above.  LevelN keys must be bound to enter odd
+-- levels>1; AltGr usually does this for level 3, further keys can be
+-- bound to 5 and 7 at least.
+--
+-- In OSX, those are called maps, and are associated to various
+-- combinations of modifiers, *including capslock*.  It is worth
+-- noting that creating a map on the command modifier does *not* turn
+-- Command-key into a character input combination.  Instead, it makes
+-- Command emit that key combined with itself. This is useful for
+-- non-latin layouts to be able to emit the standard bindings; a Greek
+-- layout could for example bind x (the letter X) to the χ (the letter
+-- chi) key on the command map, so Command-χ (Command-Chi) would emit
+-- Command-X, cut.
+--
+-- In Windows, combinations of modifiers (mapped VKs) enter a
+-- "shiftState", and a modifier table determine which shiftstate are
+-- valid for entering characters. Unlike what MacOS does, this doesn't
+-- modify bindings: if Ctrl-Alt-χ is bound to X, and χ is on VK_A,
+-- then pressing Ctrl-Alt-χ will either emit the character X, *OR*, if
+-- the application has registered it, the shortcut Ctrl-Alt-χ, *OR*,
+-- if the application is listening to VK_A, Ctrl-Alt-VK_A that gets
+-- translated to Ctrl-Alt-A.
+--
+-- A possible limitation of Windows is that the level-up key (ALtGr)
+-- is always emulated and translated to Ctrl-Alt, which may conflict
+-- with app bindings.
+--
+-- Because of this variety, Compilers will probably expose a
+-- translation function from Layer (or more exactly, [a] defaulting to
+-- [Layer]) to their preferred internal representation.
+data Layer = Layer
+  { level :: Int -- 0 for base, 1 for altGr, n… for further levels.
+  , modifiers :: ModifierMask }
   deriving (Eq, Ord, Read, Show)
 
 -- | Modifier mask for a layer.
@@ -62,23 +99,9 @@ data ModifierMask = ModifierMask
 instance Default ModifierMask where
   def = ModifierMask False Off Off Off Off
 
--- | A Layer descriptor, as a level and a state of modifiers.  MacOS
--- doesn't have levels, though.  @FIXME How to handle level>0 on mac
--- generator? Ignore entirely, treat as anyOption?  In this case ->
--- need to detect abnormal description.
---
--- On MacOS, level == 1 gets translated to an anyOption map.  It is an
--- error in the macos generator to both have keys bound in an
--- ModifierMask with option non-Off and a level>0.
---
--- Conversely, if there's no level>0 but there are keys bound in a
--- ModifierMask where command and control are off and option non-off,
--- they'll get handled as level 1 (the altGr layer, which XKB calls
--- Level 3).
-
-data Layer = Layer
-  { level :: Int -- 0 for base, 1 for altGr, n… for further levels.
-  , modifiers :: ModifierMask }
+-- | Most modifiers are duplicated left and right, and MacOS at least
+-- supports the distinction.  Windows too, I think.
+data ModifierState = Off | Any | Left | Right
   deriving (Eq, Ord, Read, Show)
 
 -- | A dumb generator for an infinite list of layers. ShitMod only.
